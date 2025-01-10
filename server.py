17344ce5ccf011ef329040a2713dbbe2eef9c3b5
Arav Patel <aravp@terpmail.umd.edu>
2025-01-10 00:33:26 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,8 +2,4 @@ from translator import app, audio_files
 import os
 
 if __name__ == "__main__":
-    app.launch(server_name="localhost", server_port=8080)
-    
-    for file in audio_files:
-        print(f"Removing {file}")
-        os.remove(f'{file}')
+    app.launch(server_name="0.0.0.0", server_port=8080)
