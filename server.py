da58e96ab6b36aba18057232ed5f8f23abc8c767
Arav Patel <aravp@terpmail.umd.edu>
2025-01-10 00:52:21 -0500

    Update server launch configuration to use environment variable for port

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ from translator import app, audio_files
 import os
 
 if __name__ == "__main__":
-    app.launch(server_name="0.0.0.0", server_port=8080)
+    
+    port = int(os.environ.get("PORT", 8080))
+    app.launch(server_name="localhost", server_port=port)
     
     for file in audio_files:
         print(f"Removing {file}")
